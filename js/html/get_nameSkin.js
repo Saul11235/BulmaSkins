6f68807dc4b55cb6670f36ba8f544a760b6fb35d
Edwin Saul <edwinsaulpm@gmail.com>
2024-01-27 15:27:45 -0500

    js fix url

--- js/html/get_nameSkin.js
+++ js/html/get_nameSkin.js
@@ -4,4 +4,9 @@ var urlParams = new URLSearchParams(url);
 var nameSkin=urlParams.get("skin")
 var isdark  =urlParams.get("dark")
 
+/* ------------------------------------ */
+
+console.log("skin");
+console.log(skin);
+
 
