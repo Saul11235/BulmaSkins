35c8787f56859f58a46cbb2d02d85bf1aa6a75d0
Edwin Saul <edwinsaulpm@gmail.com>
2024-01-28 19:31:33 -0500

    changes structure

--- js/webapp/v1_init/init.js
+++ js/webapp/v1_init/init.js
@@ -3,13 +3,37 @@ console.log("v1: view 1 init");
 
 function button_from_skin(nameSkin) {
   return `
-<a class="button is-dark"
-href ="https://saul11235.github.io/BulmaSkins/?&skin=XXX">
-  <span>
-  XXX
-  </span>
-</a>
+<div class="" 
+style="border:1px solid white;
+       border-radius: 20px;
+       margin:20px;
+       padding: 20px; ">
+  <div class="">
+    <a class="title"
+    href="https://saul11235.github.io/BulmaSkins/?&skin=XXX">
+      XXX
+    </a>
+    <br/>
+    <br/>
 
+       <div class="field has-addons">
+         <a class="button is-link is-small is-rounded"
+	   href="https://saul11235.github.io/BulmaSkins/?&skin=XXX">
+	   <span>preview</span>
+	   <span class="icon"> <i class="bi bi-globe"></i> </span>
+	 </a>
+         <a class="button is-light is-small is-rounded" 
+	   href="https://saul11235.github.io/BulmaSkins/?&skin=XXX">
+	   <span>code</span>
+	   <span class="icon"> <i class="bi bi-github"></i> </span>
+	 </a>
+
+
+
+       </div>
+ 
+  </div>
+</div>
   `.replace(new RegExp("XXX", 'g'), nameSkin);
 };
 
@@ -24,22 +48,30 @@ for (var i = 0; i < arrayNameSkins.length; i++) {
 
 
 document.body.innerHTML=`
-<div class="hero is-dark is-fullheight">
-
-  <div class="hero-head">
-     <div class="content">
-     <div class="block">
-        <h1 class="title">
+<div class="hero is-dark ">
+  <div class="hero-body">
+    <div class="content">
+      <h1 class="title">
 	  BulmaSkins
-	</h1>
-	</div>
-     </div>
+       </h1>
+       <p>
+         is an collection for themes based on
+	 <a class="is-underline">
+	     Bulma
+	 </a>
+       </p>
+   </div>
   </div>
+</div>    
 
+
+<div class="hero is-dark is-halftheight">
 
   <div class="hero-body">
     <div class="content">
-       `+buttons+`
+      <div class="columns">
+        `+buttons+`
+       </div>
     </div>
   </div> 
 
